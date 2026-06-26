4428045a60741423f53a17639eeb68e8e544bc03
Hilarion Gaona <hilarionga333@gmail.com>
2026-06-26 16:12:10 -0300

    funciones actualizadas

--- API/app.py
+++ API/app.py
@@ -35,9 +35,3 @@ def get_warriors_got():
     rows = ["Jon Snow", "Daenerys Targaryen", "Arya Stark", "Tyrion Lannister", "Cersei Lannister", "Jaime Lannister", "Bran Stark", "Sansa Stark"]
     return rows
 
-
-
-@app.get("/cursosPlatzi")
-def get_cursos():    
-    cursos = ["Docker", "Bash", "Linux", "Inglés","Python" , "Javascript", "Azure", "DevOps"]    
-    return rows
