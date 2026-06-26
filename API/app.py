e7fe3f95367e51f2c2324f3ccb2c42da579456f8
Hilarion Gaona <hilarionga333@gmail.com>
2026-06-26 17:09:03 -0300

    agrego jose a familia

--- API/app.py
+++ API/app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/familia")
 def get_familia():
-    rows = ["Amin", "Marce", "Miranda"]
+    rows = ["Amin", "Marce", "Miranda", "José"]
     return rows
 
 @app.get("/superheroesDC")
